990f651d5fd2f359126b46d22aa0bfb70794aa9c
Eli Front <elifront23@gmail.com>
2023-09-01 13:08:58 -0500

    glsl icon

--- polish.lua
+++ polish.lua
@@ -1,8 +1,18 @@
 return function()
+  local web_devicons = require "nvim-web-devicons"
+
   vim.filetype.add {
     extension = {
       ["frag"] = "glsl",
       ["vert"] = "glsl",
+    },
+  }
+
+  web_devicons.set_icon {
+    ["frag"] = {
+      icon = "△",
+      color = "#00FF00",
+      name = "Frag",
     },
   }
 end
